1fb2b6f0237988555a8b53e67fa3188ce18e4238
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-04 16:12:16 +0100

    messing around

--- demo/serializers.py
+++ demo/serializers.py
@@ -6,4 +6,4 @@ from demo.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title']
+        fields = ['title', 'description']
--- demo/urls.py
+++ demo/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import Another, BookViewSet
+from .views import Another, BookViewSet, first
 
 router = routers.DefaultRouter()
 router.register('books', BookViewSet)
 
 urlpatterns = [
     path('another', Another.as_view()),
+    path('first', first),
     path('', include(router.urls)),
 ]

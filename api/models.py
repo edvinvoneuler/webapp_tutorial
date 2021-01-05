9c0cbf4c1318fc205039218f90d608b815fa3a99
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-05 10:51:15 +0100

    Models WIP.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Game(models.Model):
+    # Attributes
+    name = models.CharField(max_length=256, unique=True)
+    steam_app_id = models.CharField(max_length=256, null=True, blank=True)
+
+    # TODO: Think about how to implement this, will it be provided through Steam API calls? If not, too manual.
+    # genre = models.ForeignKey(Genre)
+
+
+class Session(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()

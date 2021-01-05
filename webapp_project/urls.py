b9323cc89970ce9130eca40245bbdb31f15033b4
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-05 11:29:33 +0100

    fix to urls

--- webapp_project/urls.py
+++ webapp_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
-    path('api', include('api.urls')),
+    path('api/', include('api.urls')),
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
     path('auth/', obtain_auth_token)

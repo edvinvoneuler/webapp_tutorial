34868ff724ddccb59245a9f02f555c4be7eedfcf
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-05 11:30:25 +0100

    Add: player, session-related and game models + initial migration

--- api/models.py
+++ api/models.py
@@ -13,5 +13,25 @@ class Game(models.Model):
 
 
 class Session(models.Model):
+    # Attributes
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+    # Relationships
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
+
+
+class PlayerSessionLink(models.Model):
+    # This is just here in case we want to add some attributes to this relationship, like session stats per player.
+    # (plus its nicer to be explicit IMO)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='session_player_links')
+    player = models.ForeignKey('Player', on_delete=models.PROTECT, related_name='player_session_links')
+
+
+class Player(models.Model):
+    # Attributes
+    nickname = models.CharField(max_length=256)
+
+    # Relationships
+    # user = models.ForeignKey(User) # TODO: Link django users with players or create from scratch (sounds stupid)
+    sessions = models.ManyToManyField(Session, through=PlayerSessionLink)

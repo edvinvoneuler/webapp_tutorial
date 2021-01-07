72b04e65aa3f132bf86730eaf26bc60ced30b8d6
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-07 15:59:52 +0100

    Add: authentication classes and permission classes for viewsets.

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 
@@ -21,7 +22,8 @@ from api.serializers import (
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (AllowAny, )
 
     @action(detail=True, methods=['POST'])
     def set_ownership_and_rating(self, request, pk=None):

0f8224cb7956ad7a1fc61ca935298d415da82f4e
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-07 14:49:25 +0100

    Add: User and Token creation

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from api.models import Game, GameOwnership, Session, PlayerSessionLink
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class GameSerializer(serializers.ModelSerializer):
@@ -13,7 +14,13 @@ class GameSerializer(serializers.ModelSerializer):
 class UserMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'password']
+        # extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class GameOwnershipSerializer(serializers.ModelSerializer):

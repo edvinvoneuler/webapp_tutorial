9d7fd68e9cdaba760ac92a21622902759fa6e69f
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-08 17:33:11 +0100

    react intro

--- front-end/src/components/numbers.js
+++ front-end/src/components/numbers.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Numbers = () => {
+    const [numbers, setNumbers] = useState(['one', 'two', 'three'])
+    const [letters, setLetters] = useState(['a', 'b', 'c'])
+    
+    const addNumber = () => {
+        setNumbers([...numbers, 'four'])
+    }
+
+    const addLetter = () => {
+        setLetters([...letters, 'd'])
+    }
+    
+    useEffect ( () => {
+        console.log('useEffect triggered');
+    },[numbers]);
+
     return (
-        <h1>Numbers</h1>
+        <div>
+            <h1>Numbers</h1>
+            { numbers.map( num => {
+                return <h4 key={num}>{num}</h4>})}
+             <button onClick={addNumber}>Add a Number</button>
+
+             <h1>Letters</h1>
+            { letters.map( letter => {
+                return <h4 key={letter}>{letter}</h4>})}
+        <button onClick={addLetter}>Add a Letter</button>
+        </div>
     )
 };
 

75045e389698d67699915b4702663c3fbaddb06b
edvin.voneuler <edvin.voneuler@anocca.com>
2021-01-12 15:32:40 +0100

    start-react boilerplate for "real" FE.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,43 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
-import {Header} from './components/header';
-import Footer from './components/footer';
-import styled from 'styled-components';
-import Numbers from './components/numbers';
-
-function OurText(){
-  return <p>This is our text</p>
-};
-
-function createAlert() {
-  alert("Thanks for clicking me.")
-}
-
-const Paragraph = styled.p`
-  font-size: 2em;
-  color: green;
-`;
-
-function ShowMessage(props){
-  if(props.toShow){
-    return <h2>My ending Message</h2>
-  }
-  return <h2>En Ding Ding Värld</h2>
-}
 
 function App() {
+
+  const [games, setGames] = useState([]);
+
   return (
     <div className="App">
-      <Header info="This is the daily message" myAlert={createAlert}/>
-      <p>
-        Hello World
-      </p>
-      <OurText/>
-      <OurText/>
-      <Paragraph>VERY COOL</Paragraph>
-      <OurText/>
-      <Numbers/>
-      <ShowMessage toShow={true}/>
-      <Footer signature="Goodbye from Edvin"/>
+      <header className="App-header">
+          <h1>Hello this is an app</h1>
+      </header>
+      <div className="layout">
+            <div>
+              { games.map( game =>{
+                return <h2>{ game }</h2>
+              })}
+            </div>
+            <div>Two</div>
+      </div>
     </div>
   );
 }
